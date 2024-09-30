33e5e21416e547790abc84d318c306a079ba4522
KhaetbekIT <hayotbek-web@mail.ru>
2024-09-30 09:57:04 +0500

    #fix #js #redux-toolkit Fixed logic add services type and counters

--- src/redux/slices/receptionSlice.jsx
+++ src/redux/slices/receptionSlice.jsx
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const initialState = {
   services: [],
   total_amount: 0,
   checkStatus: true,
   paid_amount: 0,
-  payment_method: null,
+  payment_method: undefined,
+  analyses: [],
+  analyse_nurse: [],
 };
 
 export const receptionSlice = createSlice({
@@ -13,21 +14,28 @@ export const receptionSlice = createSlice({
   initialState,
   reducers: {
     addService(state, action) {
-      state.services = [...state.services, action.payload];
+      const { type, data } = action.payload;
+      state.total_amount += parseFloat(data.amount);
 
-      if (state.services.length > 0) {
-        state.total_amount += parseFloat(action.payload.amount);
-      }
+      if (type === "SERVICES") state.services.push(data);
+      else if (type === "ANALYSES") state.analyses.push(data);
+      else if (type === "ANALYSE_NURSE") state.analyse_nurse.push(data);
     },
     paidAmount(state, action) {
       state.paid_amount = action.payload;
     },
     removeServiceItem(state, action) {
-      state.services = action.payload;
-      state.total_amount = action.payload.reduce(
-        (acc, cur) => acc + cur.amount,
-        0,
-      );
+      const { type, filtered } = action.payload;
+
+      if (type === "SERVICES") state.services = filtered;
+      else if (type === "ANALYSES") state.analyses = filtered;
+      else if (type === "ANALYSE_NURSE") state.analyse_nurse = filtered;
+
+      state.total_amount = [
+        ...state.services,
+        ...state.analyses,
+        ...state.analyse_nurse,
+      ].reduce((acc, cur) => acc + cur.amount, 0);
     },
     saveServices(state) {
       state.checkStatus = false;
