ff9841951748334825979d6e4d5ec5a987a9bf92
KhaetbekIT <hayotbek-web@mail.ru>
2024-10-11 22:29:35 +0500

    #fix #js #next #storybook #libs #ui #old-version

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+    "@storybook/addon-themes"
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+/** @type { import('@storybook/react').Preview } */
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        // nameOfTheme: 'classNameForTheme',
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
+};
+
+export default preview;
